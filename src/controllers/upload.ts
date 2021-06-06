f74bf330368f2c68715576834f54bfe619fa0a59
ehsan <ehsan@raychat.io>
2021-06-06 21:16:53 +0430

    refactor uload controller

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -1,94 +1,68 @@
 // Packages
 import * as _ from "lodash";
 import { Request, Response } from "express";
+import * as fs from 'fs';
+import * as path from 'path';
 
 // Models
 import UploadModel from "../models/upload";
 
 // Helper
-import { signator } from './../helpers/jwt';
+import Multer from '../helpers/multer';
 
 // Consts
 enum Consts {
-    PERFIX = 'upload'
+  PERFIX = 'upload'
 }
 
 /**
  * TODO Conversation Controller
  */
 class Upload {
-    /**
-     * TODO Constructor
-     */
-    constructor() { }
+  /**
+   * TODO Constructor
+   */
+  constructor() { }
 
-    /**
-     * TODO Create Controller
-     * @param {request} req
-     * @param {response} res
-     */
-    async create(req: Request, res: Response) {
-        try {
-            const created: any = await UploadModel.create(req.body);
-            return res.send({ success: true, created });
-        } catch (err) {
-            return res.status(500).send(err.message);
-        }
+  /**
+   * TODO Create Controller
+   * @param {request} req
+   * @param {response} res
+   */
+  async upload(req: Request, res: Response) {
+    try {
+      const files: any = await Multer.upload(req);
+      files.map(async item => {
+        await UploadModel.create(item);
+      });
+      return res.send({ success: true, data: files });
+    } catch (err) {
+      return res.status(500).send(err.message);
     }
+  }
 
-    /**
-     * TODO Update Controller
-     * @param {request} req
-     * @param {response} res
-     */
-    async update(req: Request, res: Response) {
-        try {
-            const _id = req.params.id;
-            const updated: any = await UploadModel.findOneAndUpdate({ _id }, { $set: req.body }, { new: true });
-            return res.send({ success: true, updated });
-        } catch (err) {
-            return res.status(500).send(err);
+  /**
+   * TODO Find One Controller
+   * @param {request} req
+   * @param {response} res
+   */
+  async show(req: Request, res: Response) {
+    try {
+      const cdnFile: string = req.params.name;
+      const getFile: any = await UploadModel.findOne({ cdnFile });
+      if (!getFile) {
+        return res.status(404).send({ success: false });
+      }
+      fs.readFile(path.join(__dirname, `./../../uploads/${cdnFile}`), (err, data) => {
+        if (err) {
+          return res.status(500).send(err);
         }
+        res.type(getFile.mimeType).send(data);
+      });
+    } catch (err) {
+      return res.status(500).send(err);
     }
-    /**
-     * TODO Delete Controller
-     * @param {request} req
-     * @param {response} res
-     */
-    async delete(req: Request, res: Response) {
-        try {
-            const deleted: any = await UploadModel.deleteOne({ _id: req.params.id });
-            return res.send({ success: true, deleted });
-        } catch (err) {
-            return res.status(500).send(err);
-        }
-    }
-
-    /**
-     * TODO Find Controller
-     * @param {request} req
-     * @param {response} res
-     */
-    async find(req: Request, res: Response) {
-        try {
-            // const {} = ta inja
-            return res.send({ success: true });
-        } catch (err) {
-            return res.status(500).send(err);
-        }
-    }
-    /**
-     * TODO Find One Controller
-     * @param {request} req
-     * @param {response} res
-     */
-    async findOne(req: Request, res: Response) {
-        try {
-            return res.send({ success: true });
-        } catch (err) {
-            return res.status(500).send(err);
-        }
-    }
+  }
 }
 
 export default Upload;
