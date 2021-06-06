e4887384f9a47cca5c803c3faff46c938cda0e88
ehsan <ehsan@raychat.io>
2021-06-06 21:19:09 +0430

    add route to authorization

--- src/routes/authorization.ts
+++ src/routes/authorization.ts
@@ -10,14 +10,16 @@ import { signIn, signUp } from "./../middlewares/authorization";
 // Consts
 const router = Router();
 enum Names {
-    ROOT = "/signin",
-    PARAM = "/signup",
+  ROOT = "/signin",
+  PARAM = "/signup",
+  FIND = '/:id'
 }
 const Authorization: any = new AuthorizationController();
 
 router
-    .post(Names.ROOT, signIn, Authorization.signIn)
-    .post(Names.PARAM, signUp, Authorization.signUp);
+  .post(Names.ROOT, signIn, Authorization.signIn)
+  .post(Names.PARAM, signUp, Authorization.signUp)
+  .get(Names.FIND, Authorization.find);
 
 export default router;
 
