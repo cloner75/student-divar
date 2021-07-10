e846ee824c89f7ae4ac91827d73457336d6b6b75
ehsan <ehsan@raychat.io>
2021-07-11 01:16:06 +0430

    check user type for dashboard

--- view/auth/helper/index.js
+++ view/auth/helper/index.js
@@ -1,32 +1,33 @@
 const Setting = {
-    address: 'http://localhost:3000/',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29yZSI6eyJzdW0iOjEwMCwiZGlzdGFuY2UiOjEwLCJncmFkZVBvaW50QXZlcmFnZSI6MjAsInF1b3RhIjo0MCwidGVybSI6MzB9LCJzdGF0dXMiOiIxIiwiaXNVc2VyIjp0cnVlLCJfaWQiOiI2MDMwZGIxNGYzNGNlNjRmMzFjNjU3MDIiLCJyZW50Ijp7InBhaWQiOmZhbHNlfSwiZW1haWwiOiJlaHNhbkByYXljaGF0LmlvIiwidXNlcm5hbWUiOiJlaHNhbiIsInVwZGF0ZWRBdCI6IjIwMjEtMDItMjBUMDk6NDk6MDguMTk3WiIsImNyZWF0ZWRBdCI6IjIwMjEtMDItMjBUMDk6NDk6MDguMTk3WiIsImlhdCI6MTYxNDMzODIyNCwiZXhwIjoxNjE0NDI0NjI0fQ.mGc9ubWtI6mQvLq87GgyK2-q58H0NeW_dJ_FNhKLL7g'
+  address: 'http://localhost:3000/',
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29yZSI6eyJzdW0iOjEwMCwiZGlzdGFuY2UiOjEwLCJncmFkZVBvaW50QXZlcmFnZSI6MjAsInF1b3RhIjo0MCwidGVybSI6MzB9LCJzdGF0dXMiOiIxIiwiaXNVc2VyIjp0cnVlLCJfaWQiOiI2MDMwZGIxNGYzNGNlNjRmMzFjNjU3MDIiLCJyZW50Ijp7InBhaWQiOmZhbHNlfSwiZW1haWwiOiJlaHNhbkByYXljaGF0LmlvIiwidXNlcm5hbWUiOiJlaHNhbiIsInVwZGF0ZWRBdCI6IjIwMjEtMDItMjBUMDk6NDk6MDguMTk3WiIsImNyZWF0ZWRBdCI6IjIwMjEtMDItMjBUMDk6NDk6MDguMTk3WiIsImlhdCI6MTYxNDMzODIyNCwiZXhwIjoxNjE0NDI0NjI0fQ.mGc9ubWtI6mQvLq87GgyK2-q58H0NeW_dJ_FNhKLL7g'
 };
 
 function signIn(email, password, url) {
-    axios.post(Setting.address.concat('authorization/signin'), {
-        email,
-        password
+  axios.post(Setting.address.concat('authorization/signin'), {
+    email,
+    password
+  })
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('isUser', res.data.user.isUser);
+      window.location.href = url;
     })
-        .then(res => {
-            localStorage.setItem('token', res.data.token);
-            window.location.href = url;
-        })
-        .catch(err => {
-            alert('نام کاربری یا رمز عبور اشتباه است');
-            console.log(`error : ${err.message}`);
-        });
+    .catch(err => {
+      alert('نام کاربری یا رمز عبور اشتباه است');
+      console.log(`error : ${err.message}`);
+    });
 }
 
 function signUp(email, password, url) {
-    alert(url)
-    axios.post(`${Setting.address}authorization/signup`, {
-        password,
-        email
+  alert(url);
+  axios.post(`${Setting.address}authorization/signup`, {
+    password,
+    email
+  })
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      window.location.href = url;
     })
-        .then(res => {
-            localStorage.setItem('token', res.data.token);
-            window.location.href = url;
-        })
-        .catch(err => alert(`error: ${err.message}`));
+    .catch(err => alert(`error: ${err.message}`));
 }
